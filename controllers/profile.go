6f973259a50cd88b23e79688aa4b009828059bc3
DYernar <eduisebai.1@gmail.com>
2021-02-21 21:02:21 +0600

    extension and size check

--- controllers/profile.go
+++ controllers/profile.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 func Profile(w http.ResponseWriter, r *http.Request) {
@@ -54,17 +55,31 @@ func Profile(w http.ResponseWriter, r *http.Request) {
 				}
 
 				var extension string
-				for i := 0; i < len(handler.Filename); i++ {
-					if handler.Filename[i] == '.' {
-						for j := i; j < len(handler.Filename); j++ {
-							extension = extension + string(handler.Filename[j])
-						}
-						break
-					}
+				var arr = strings.Split(handler.Filename, ".")
+				extension = arr[len(arr)-1]
+				fmt.Print("EXTENSION: ")
+				fmt.Println(extension)
+
+				if extension != "jpeg" && extension != "png" && extension != "gif" && extension != "jpg" {
+					var allData model.AllData
+					allData.User = database.GetUserByName(uname)
+					allData.User.Posts = database.GetPostsByUserID(allData.User.UserID)
+					allData.Categories = []string{"Books", "Movies", "Music", "Sport"}
+					t := template.Must(template.New("profile").ParseFiles("static/profile.html", "static/header.html"))
+					allData.Error = "invalid image type it can be only: jpeg, png, gif or jpg"
+					var newPost model.Post
+					user := database.GetUserByName(uname)
+					newPost.UserID = user.UserID
+					newPost.Text = r.FormValue("text")
+					newPost.Title = r.FormValue("title")
+					fmt.Println(newPost.Text)
+					allData.CurrentPost = newPost
+					t.Execute(w, allData)
+					return
 				}
+
 				lastPost := database.GetLastPost()
 				name = strconv.Itoa(lastPost) + extension
-				fmt.Print(name)
 
 				fileBytes, _ := ioutil.ReadAll(file)
 
